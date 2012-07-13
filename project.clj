30683d24a0f9e3150cc36a1e60d9b40b2aac5196
Logan Campbell-McPherson <colin@logaan.net>
2012-07-13 14:49:59 +1000

    lein 2 project with datomic dependancy

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject lists-of-things "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.datomic/datomic "0.1.3164"]])
---
+++ src/lists_of_things/core.clj
@@ -0,0 +1,1 @@
+(ns lists-of-things.core)
