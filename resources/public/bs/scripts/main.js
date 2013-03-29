75d98ce56652addf083bcbe40d524402418b2580
Logan Campbell <colin@logaan.net>
2013-03-29 15:21:50 +1100

    add parent popover displays

--- resources/public/bs/scripts/main.js
+++ resources/public/bs/scripts/main.js
@@ -134,6 +134,15 @@ Path.map("/bs/things/:thing_id").to(function(){
   loadThing("/things/" + this.params.thing_id);
 });
 
+ko.bindingHandlers.bootstrapPopover = {
+  init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+    var options = valueAccessor();
+    var defaultOptions = {placement: 'bottom', html: true};
+    options = $.extend(true, {}, defaultOptions, options);
+    $(element).popover(options);
+  }
+};
+
 jQuery(function() {
   Path.history.listen();
 
