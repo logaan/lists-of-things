3d981c21683f4c3a9858407ecd5cd103da16479c
Logan Campbell <colin@logaan.net>
2013-04-01 22:51:39 +1100

    rethinking how to implement thing creation

--- resources/public/bs/scripts/page.js
+++ resources/public/bs/scripts/page.js
@@ -4,6 +4,9 @@ function Page(page) {
 
     preview: ko.observable(page.preview || page.listing),
 
+    // Setup the thing name field as bound to an UnsavedThing object. It will
+    // have the 'create' function on it rather than that sitting in with things
+    // from listings. Might be a good opportunity to introduce UUIDs.
     addThing: function(form) {
       var thing = Thing({
         name: $("#new-thing-name", form).val()
