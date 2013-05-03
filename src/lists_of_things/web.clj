76d247cd6945cc9dc0f0e2b2de681a3f6814be2b
Logan Campbell <colin@logaan.net>
2013-05-04 00:39:16 +1000

    storing email in session

--- src/lists_of_things/web.clj
+++ src/lists_of_things/web.clj
@@ -3,6 +3,7 @@
   (:use [compojure.core :only [defroutes GET POST DELETE PUT]])
   (:require
     [clj-http.lite.client :as http]
+    [cheshire.core :as json]
     [lists-of-things
      [api :as api]
      [helpers :as helpers]
@@ -12,10 +13,13 @@
      [handler :as handler]
      [core :as compojure]
      [route :as route]]
+    [ring.util
+     [response :as response]]
     [ring.adapter
      [jetty :as jetty]]
     [ring.middleware
-     [gzip :as gzip]]))
+     [gzip :as gzip]
+     [session :as session]]))
 
 (def api-controller
   (-> api/routes
@@ -25,13 +29,21 @@
 (def interface-controller
   interface/routes)
 
+(defn verify [assertion]
+  (http/post "https://verifier.login.persona.org/verify"
+             {:form-params {:assertion assertion
+                            :audience "http://localhost:3000"}}))
+
 (defroutes bootstrap
-  (POST "/sign-in" [assertion]
-    (http/post "https://verifier.login.persona.org/verify"
-                 {:form-params {:assertion assertion
-                                :audience "http://localhost:3000"}}))
+  (POST "/sign-in" {session :session {assertion :assertion} :params} 
+    (let [{body :body}    (verify assertion)
+          {email "email"} (json/parse-string body)
+          new-session     (assoc session :email email)]
+      (assoc (response/response
+               (str "Old" (:email session) "New" email))
+             :session new-session)))
   (GET "/bs/things" []
-    (slurp "resources/public/bs/index.html")) 
+    (slurp "resources/public/bs/index.html"))
   (GET "/bs/things/:id" [id]
     (slurp "resources/public/bs/index.html")))
 
@@ -42,7 +54,12 @@
   (route/resources "/")
   (route/not-found helpers/not-found))
 
-(def app-controller (-> routes handler/site wrappers/connection gzip/wrap-gzip))
+(def app-controller
+  (-> routes
+      handler/site
+      wrappers/connection
+      gzip/wrap-gzip
+      session/wrap-session))
 
 (defn -main [& args]
   (jetty/run-jetty app-controller {:port 3000}))
