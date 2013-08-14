a8f1c715c5be4a393075aac90dc844b3667d7732
Logan Campbell <colin@logaan.net>
2013-08-15 06:04:40 +1000

    content markdown live preview

--- resources/public/scripts/new_content.js
+++ resources/public/scripts/new_content.js
@@ -3,6 +3,14 @@ function NewContent(parentThing) {
 
   my.text = ko.observable("");
 
+  my.previewVisible = ko.computed(function() {
+    return my.text().length > 0;
+  });
+
+  my.preview = ko.computed(function() {
+    return my.previewVisible() ? markdown.toHTML(my.text()) : "";
+  });
+
   my.add = function() {
     repository.addContent(parentThing.id(), my.text());
   };
